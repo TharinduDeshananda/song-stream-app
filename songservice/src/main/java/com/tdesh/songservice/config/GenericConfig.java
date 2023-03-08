864ec26734c39615f46c6b68a5a6643c5bbd3cdc
TharinduDeshananda <tdeshananda@gmail.com>
2023-03-08 20:59:18 +0530

    update

--- songservice/src/main/java/com/tdesh/songservice/config/GenericConfig.java
+++ songservice/src/main/java/com/tdesh/songservice/config/GenericConfig.java
@@ -1,9 +1,13 @@
 package com.tdesh.songservice.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.reactive.function.client.*;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GenericConfig {
@@ -11,7 +15,19 @@ public class GenericConfig {
     @Bean
     @LoadBalanced
     public WebClient.Builder getWEbClientBuilder(){
-        return WebClient.builder();
+        return WebClient.builder().filter(new ExchangeFilterFunction() {
+            @Override
+            public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
+
+                HttpServletRequest httpServletRequest = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+                String authHeader = httpServletRequest.getHeader("Authorization");
+                System.out.println("Auth header received with value: "+authHeader);
+                ClientRequest newRequest = ClientRequest.from(request)
+                        .header("Authorization",authHeader)
+                        .build();
+                return next.exchange(newRequest);
+            }
+        });
     }
 
 }
---
+++ userservice/src/main/java/com/tdesh/userservice/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.tdesh.userservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .oauth2ResourceServer(r->r.jwt(j->j.jwkSetUri("http://localhost:9090/oauth2/jwks")))
+
+                .authorizeHttpRequests().anyRequest().authenticated().and()
+                .build();
+    }
+}
